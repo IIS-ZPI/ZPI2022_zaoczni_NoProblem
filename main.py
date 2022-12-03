79895e3fc66bdc166c2145ef5e700ef8ecf62077
pawel <pawelpatyk@interia.com>
2022-12-03 09:13:07 +0100

    division added

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-print('NAZWA_GRUPY No Problem, ID DanielWap, ID Developer ppatyk')
+#print('NAZWA_GRUPY No Problem, ID DanielWap, ID Developer ppatyk')
 
+def Division (A, B):
+    if(B==0):
+        print('nie dziel przez 0!')
+        return 0.0
+    else:
+        return A/B
+
+
+
+A = float(input("Enter Number A : "))
+B = float(input("Enter Number B : "))
+result = Division(A,B)
+print(A, " / ", B, " = ", result)
 
 
 
