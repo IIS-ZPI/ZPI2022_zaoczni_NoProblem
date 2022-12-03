a012d8923fb663f945cec6d91ddcbf07237c644e
229258 <229258@edu.p.lodz.pl>
2022-12-03 15:43:54 +0100

    Added difference operation.

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,3 +1,8 @@
 class AddOperator():
     def add(self, first_value, second_value):
         return first_value + second_value
+
+
+class DiffOperator():
+    def diff(self, first_value: float, second_value: float) -> float:
+        return first_value - second_value
