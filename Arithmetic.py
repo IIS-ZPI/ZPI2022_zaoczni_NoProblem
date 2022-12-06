d908dce17300e7fb29d89b4d5ede9bc8cccf1f34
229258 <229258@edu.p.lodz.pl>
2022-12-06 19:48:13 +0100

    added comments

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,18 +1,22 @@
+# addition
 class AddOperator:
     def add(self, first_value, second_value):
         return first_value + second_value
 
 
+# multiplication
 class MultiplyOperator:
     def multiply(self, first_value, second_value):
         return first_value * second_value
 
 
+# subtraction
 class DiffOperator:
     def diff(self, first_value: float, second_value: float) -> float:
         return first_value - second_value
 
 
+# division
 class DivisionOperator:
     def divide(self, first_value, second_value):
         if second_value == 0:
