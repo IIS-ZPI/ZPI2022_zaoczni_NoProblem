546bec4b8eb78291ed7dfd8c4168010226afe9b3
ppatyk <78248344+ppatyk@users.noreply.github.com>
2022-12-09 21:18:29 +0100

    Revert "Ex. 6 feature added"

--- Arithmetic.py
+++ Arithmetic.py
@@ -4,21 +4,18 @@ class AddOperator:
         return first_value + second_value
 
 
-#Wiktor's class
 # multiplication
 class MultiplyOperator:
     def multiply(self, first_value, second_value):
         return first_value * second_value
 
 
-#Wojtek's class
 # subtraction
 class DiffOperator:
     def diff(self, first_value: float, second_value: float) -> float:
         return first_value - second_value
 
 
-#Pawel's class
 # division
 class DivisionOperator:
     def divide(self, first_value, second_value):
@@ -26,12 +23,3 @@ class DivisionOperator:
             raise Exception("Nie dziel przez zero!")
         else:
             return first_value/second_value
-
-
-class AbsOperator:
-    def abs (self,a,b):
-        if a<0:
-            a = a*(-1)
-        if b<0:
-            b= b*(-1)
-        return(a,b)
